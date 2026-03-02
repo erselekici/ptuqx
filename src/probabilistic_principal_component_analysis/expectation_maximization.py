5207286bdf4b70a945a35bc95940eec29c3ba6c5
Eren Ekici <ge95bok@mytum.de>
2026-03-02 16:01:04 +0100

    feat: add expectation maximization module for PPCA

---
+++ src/probabilistic_principal_component_analysis/expectation_maximization.py
@@ -0,0 +1,3 @@
+"""Module for Expectation Maximization in Probabilistic Principal Component Analysis."""
+
+#TODO: Implement the Expectation Maximization algorithm for Probabilistic Principal Component Analysis (PPCA).
